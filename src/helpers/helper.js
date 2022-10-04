d85f9b92cf211242aee40e2aa1f0a3010bda2047
Pedro Ginel <pedroginel@hotmail.com>
2022-10-04 20:45:41 -0300

    arreglando la interaccion que volvia negativo el stock

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,3 +1,4 @@
+import Swal from "sweetalert2";
 export const setLocalStorage = (data) => {
   
   const item = data;
@@ -5,10 +6,22 @@ export const setLocalStorage = (data) => {
   
   const filter = getLocal.findIndex((x) => x._id === data._id)
   
-  item.quantity ++
+  
   if (filter !== -1) {
-    getLocal[filter] = item
+    if (getLocal[filter].stock>0) {
+      getLocal[filter].quantity ++
+      getLocal[filter].stock --
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Opps...',
+        text: `No puede llevar menos de un articulo`,
+        confirmButtonColor : "#0B5ED7",
+      })
+    }
   } else {
+    item.quantity ++
+    item.stock --
     getLocal.push(item)
   }
   localStorage.setItem('cart', JSON.stringify(getLocal))
